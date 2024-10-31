9f9f893ec27cf41ecc672658db3dba63f56364b0
Vaibhavi Kulkarni <vaibhaviact22@gmail.com>
2024-10-31 16:17:55 +0530

    Updated middleware for authentication and authorization

--- middleware.js
+++ middleware.js
@@ -7,14 +7,14 @@ module.exports.isLoggedIn = (req,res ,next)=>{
     req.session.redirectUrl = req.originalUrl;
     if (!req.isAuthenticated()){
         req.flash("error","you must be logged in to create new listings");
-        res.redirect("/login");
+        return res.redirect("/login");
     }
     next();
 };
 
 module.exports.saveRedirectUrl = (req,res ,next)=>{
     if(req.session.redirectUrl){
-        res.locals.redirectUrl=req.session.redirectUrl;
+        return res.locals.redirectUrl=req.session.redirectUrl;
     }
     next();
 };
